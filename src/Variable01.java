9ecde5efc99dead2442dbaf85521155c91e0e626
shun2015 <dwat.pmjg@gmail.com>
2021-05-17 16:37:44 +0900

    initial commit

---
+++ src/Variable01.java
@@ -0,0 +1,14 @@
+
+public class Variable01 {
+	public static void main (String[] args) {
+		int num1 = 10;
+		String str1 = "hello";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		str1 = "こんにちは";
+		System.out.println(num1);
+		System.out.println(str1);
+	}
+}
